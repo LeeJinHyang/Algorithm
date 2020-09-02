8038acf56d4234399fc8b7dd200aa812ec8d8fab
LEEJINHAYNG <wlsgid2378@naver.com>
2020-09-02 18:06:18 +0900

    DP

---
+++ 백준/9월/0902/src/nX2타일링_11726/Main.java
@@ -0,0 +1,20 @@
+package nX2≈∏¿œ∏µ_11726;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] dp = new int[1001];
+		dp[1] = 1;
+		dp[2] = 2;
+		for(int i = 3; i <= N; i++ ) {
+			dp[i] = (dp[i-1]+dp[i-2])%10007;
+		}
+		System.out.println(dp[N]);
+	}
+
+}
