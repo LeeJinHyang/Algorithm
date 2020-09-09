1adce95ad52bde03e2dce7fb37a946020122f9f3
이진향 <wlsgid2378@naver.com>
2020-09-09 20:08:20 +0900

    DP

---
+++ 백준/9월/0909/src/카드구매하기_11052/Main.java
@@ -0,0 +1,27 @@
+package 카드구매하기_11052;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] arr = new int[1001];
+		for(int i = 1; i <= N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		int[] dp = new int[1001];
+		Arrays.fill(dp,0 );
+		for(int i = 1; i <= N;i++ ) {
+			for(int j = 1; j <= i; j++ ) {
+				dp[i] = Math.max(dp[i],dp[i-j]+arr[j]);
+			}
+			
+		}
+		System.out.println(dp[N]);		                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+	}
+
+}
