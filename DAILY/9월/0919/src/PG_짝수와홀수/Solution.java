becd12ffeb16126564c9722e953823d1388715a2
LEEJINHYANG <wlsgid2378@naver.com>
2020-09-19 19:37:18 +0900

    LEVEL 1

---
+++ DAILY/9월/0919/src/PG_짝수와홀수/Solution.java
@@ -0,0 +1,10 @@
+package PG_짝수와홀수;
+
+class Solution {
+    public String solution(int num) {
+        String answer = "";
+        if(num % 2 == 0) answer = "Even";
+        else answer = "Odd";
+        return answer;
+    }
+}
