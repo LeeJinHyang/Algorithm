f9083f27e2fb8a806e66ebf67b895a29cba82d95
LEEJINHAYNG <wlsgid2378@naver.com>
2020-09-30 22:33:45 +0900

    [+] update LEVEL@ 최솟값만들기

---
+++ DAILY/9월/0930/src/PG_최솟값만들기/Solution.java
@@ -0,0 +1,18 @@
+package PG_최솟값만들기;
+
+import java.util.*;
+class Solution
+{
+    public int solution(int []A, int []B)
+    {
+        int answer = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+        for(int i = 0; i < A.length; i++){
+            answer += A[i] * B[B.length - i-1];
+            
+        }
+
+        return answer;
+    }
+}
