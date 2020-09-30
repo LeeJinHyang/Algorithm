13a14e3224f5b58b75b194f5398dedd98f1a5dea
LEEJINHAYNG <wlsgid2378@naver.com>
2020-09-30 22:00:53 +0900

    [+] update LEVEL 2 올바른 괄호

---
+++ DAILY/9월/0930/src/PG_올바른괄호/Solution.java
@@ -0,0 +1,26 @@
+package PG_올바른괄호;
+
+import java.util.*;
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == '('){
+                stack.push('(');
+            }
+            else {
+                if(!stack.isEmpty()){
+                   stack.pop(); 
+                }
+                else return false;
+                
+            }
+            
+        }
+        if(!stack.isEmpty()) return false;
+        else return  true;
+
+    }
+}
