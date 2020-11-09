f3e50063c49100f63572f5a4bf1e32772303d5e1
이진향 <wlsgid2378@naver.com>
2020-11-09 19:31:44 +0900

    [+]update 주식가격

---
+++ DAILY/11월/1109/src/PG_주식가격/Solution.java
@@ -0,0 +1,19 @@
+package PG_주식가격;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        for(int i = 0; i < prices.length-1; i++){
+            int count = 0;
+            for(int j = i+1; j < prices.length; j++){
+                answer[i] = j - i;
+                    
+                if((prices[i] > prices[j])){
+                    break;
+                }
+                
+            }
+        }
+        return answer;
+    }
+}
