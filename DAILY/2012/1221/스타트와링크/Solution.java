a7d2ab3f934dc03b4f52ef33e12eeab264493212
LEEJINHYANG <wlsgid2378@naver.com>
2020-12-21 22:31:17 +0900

    [+] add 스타트와링크

---
+++ DAILY/2012/1221/스타트와링크/Solution.java
@@ -0,0 +1,12 @@
+/*첫째 줄에 N(4 ≤ N ≤ 20, N은 짝수)이 주어진다. 
+둘째 줄부터 N개의 줄에 S가 주어진다. 
+각 줄은 N개의 수로 이루어져 있고, i번 줄의 j번째 수는 Sij 이다. 
+Sii는 항상 0이고, 나머지 Sij는 1보다 크거나 같고, 100보다 작거나 같은 정수이다.
+*/
+
+public class Solution{
+
+    public static void main(String[] args) {
+     
+}
+}
