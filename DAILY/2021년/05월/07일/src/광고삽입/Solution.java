0036948d60d0d074fe800ab0f8bcd9b01878d348
LeeJinHyang <wlsgid2378@naver.com>
2021-05-07 00:18:38 +0900

    0507 광고삽입

---
+++ DAILY/2021년/05월/07일/src/광고삽입/Solution.java
@@ -0,0 +1,15 @@
+package 광고삽입;
+
+public class Solution {
+    static public String solution(String play_time, String adv_time, String[] logs) {
+        String answer = "";
+        //해당 동영상의 어떤 구간을 재생했는지 알 수 있는 재생 구간 기록을 구함
+        //해당 기록을 바탕으로 공익광고가 삽입 될 최적의 위치를 구할 수 있다
+        
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
