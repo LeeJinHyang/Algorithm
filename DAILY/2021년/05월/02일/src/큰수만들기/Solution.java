157c6841dc201c85bb8ccfc2ba26fab1232b3559
LeeJinHyang <wlsgid2378@naver.com>
2021-05-21 22:56:45 +0900

    0521

--- DAILY/2021년/05월/02일/src/큰수만들기/Solution.java
+++ DAILY/2021년/05월/02일/src/큰수만들기/Solution.java
@@ -1,2 +1,33 @@
-package 큰수만들기;public class Solution {
+package 큰수만들기;
+
+class Solution {
+    public String solution(String number, int k) {
+        String answer = "";
+        int num = number.length() - k;
+        //n개를 삭제해 가장 큰 수 만들기
+
+        // for i를 0 ~ number.length - num 까지 돌며 가장 큰 수를 찾기
+        // 가장 큰 수의 index   부터 0 ~ number.length - num +1 까지 돌며 가장 큰 수 찾기
+        //-> num 회 반복
+        int startIdx = 0;
+        int endIdx = number.length() - num+1;
+        for(int i = 0; i < num; i++){
+            int idx = findMaxIndex(number, startIdx, endIdx);
+            answer += number.charAt(idx);
+            startIdx = idx+1;
+            endIdx++;
+        }
+
+
+        return answer;
+    }
+    static int findMaxIndex(String number, int startIdx, int endIdx){
+        int idx =startIdx;
+        for(int i = startIdx; i < endIdx; i++){
+            if(number.charAt(idx) < number.charAt(i)){
+                idx = i;
+            }
+        }
+        return idx;
+    }
 }
