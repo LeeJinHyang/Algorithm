ac08642d22a2e1c0fd29d9f15073f55af3a2173d
LeeJinHyang <wlsgid2378@naver.com>
2021-05-29 23:04:09 +0900

    0529

---
+++ DAILY/2020년/12월/0204/src/Solution1.java
@@ -0,0 +1,12 @@
+
+public class Solution1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuilder std = new StringBuilder();
+		std.append("0123456");
+		std.delete(0, 2);
+		System.out.println(std.toString());
+	}
+
+}
