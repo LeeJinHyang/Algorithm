28024156c7d7bf3d9c5d3e082533c5584cd462e9
LeeJinHyang <wlsgid2378@naver.com>
2021-06-14 00:21:59 +0900

    union-find

---
+++ DAILY/2021년/06월/14일/src/UnionFind/Solution.java
@@ -0,0 +1,24 @@
+package UnionFind;
+
+public class Solution {
+    static int[] parent;
+    static public int findParent(int x){
+        if(parent[x]== x) {
+            return x;
+        }
+        else return findParent(parent[x]);
+    }
+    static public void union(int x, int y){
+        int parentX = findParent(x);
+        int parentY = findParent(y);
+        parent[x] = parentY;
+    }
+    public static void main(String[] args) {
+        int N = 10;
+        parent = new int[N];
+        for(int i = 0; i < N; i++){
+            parent[i] = i;
+        }
+        
+    }
+}
