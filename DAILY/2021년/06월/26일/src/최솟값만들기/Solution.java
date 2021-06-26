036d72221efb18348bf069052832edffc543c477
LeeJinHyang <wlsgid2378@naver.com>
2021-06-26 12:43:47 +0900

    0626 최솟값만들기

---
+++ DAILY/2021년/06월/26일/src/최솟값만들기/Solution.java
@@ -0,0 +1,20 @@
+package 최솟값만들기;
+
+import java.util.*;
+
+class Solution
+{
+    public int solution(int []A, int []B)
+    {
+        int answer = 0;
+        //A의 min 과 B의 max를 곱해야 한다
+        Arrays.sort(A);
+        Arrays.sort(B);
+        // Arrays.sort(B, Collections.reverseOrder()); -> Integer[] 일 때 사용 가능하다
+        
+        for(int i = 0; i < A.length; i++){
+            answer += A[i] * B[B.length - 1 - i];
+        }
+        return answer;
+    }
+}
