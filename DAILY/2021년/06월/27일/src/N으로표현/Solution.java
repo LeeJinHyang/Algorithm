e6d91fabaa0d13de44152bf7c12208de63e288b7
LeeJinHyang <wlsgid2378@naver.com>
2021-06-27 22:48:04 +0900

    0627 N으로 표현

---
+++ DAILY/2021년/06월/27일/src/N으로표현/Solution.java
@@ -0,0 +1,20 @@
+package N으로표현;
+
+import java.util.*;
+class Solution {
+    public int solution(int N, int number) {
+        int answer = 0;
+        List<int[]> dp = new LinkedList<>();
+        //dp[0] -> 단순 N add
+        //dp[1] -> 덧셈
+        //dp[2] -> 곱셈
+        //dp[3] -> 나눗셈
+        //dp[4] -> 나머지
+        while(true){
+            int[] newArray = new int[5];
+
+        }
+
+        return answer;
+    }
+}
